c276f11bf3d44e085eef453e18c3d4bfbddce482
saizdx2010 <saiprashanth02012001@gmail.com>
2022-01-19 10:26:23 +0700

    Changed the styling for the span

--- src/NavItem.js
+++ src/NavItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavLinks, NavLinksContainer } from './Style';
+import { NavLinks, NavLinksContainer, NavLogo } from './Style';
 
 
 export class NavItem extends React.Component {
     render() {
         return (
             <>
-                <span className="navbar-logo">{this.props.navLogo}</span>
+                <NavLogo>{this.props.navLogo}</NavLogo>
                 <NavLinksContainer>
                     <NavLinks>{this.props.item1}</NavLinks>
                     <NavLinks>{this.props.item2}</NavLinks>
--- src/Style.js
+++ src/Style.js
@@ -3,28 +3,33 @@ import styled from "styled-components";
 const NavStyles = styled.nav`
     display: flex;
     flex-direction : row;
-    justify-content : space-around;
+    justify-content : space-between;
     width: 100%;
     height: 70px;
     align-items: center;
     background: black;
     color: white;
-    
     font-size: 1.4rem;
     
     
-`
+    `
+
+const NavLogo = styled.span`
+        margin: 0 50px;
+    `
 
 const NavLinksContainer = styled.ul`
     list-style: none;
     display: flex;  
-    float: right;
-`
+    
+    margin: 0 50px;
+    `
 
 const NavLinks = styled.li`
+    
     margin: 10px;
 `
 
 
 
-export { NavStyles, NavLinksContainer, NavLinks }
+export { NavStyles, NavLinksContainer, NavLinks, NavLogo }
